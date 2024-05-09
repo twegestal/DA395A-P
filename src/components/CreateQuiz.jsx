6f60f9b1cfb49b2ebe17b4d607a30140662b8dbc
Axel nilsson <axel.nilsson01@gmail.com>
2024-05-09 14:27:23 +0200

    Loading for quiz creation

--- src/components/CreateQuiz.jsx
+++ src/components/CreateQuiz.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/prop-types */
 import { TextInput, Button, Group, Paper, Radio, Text } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { IconSquarePlus } from '@tabler/icons-react';
 
-export const CreateQuiz = () => {
+export const CreateQuiz = ({ fetchQuiz }) => {
   const form = useForm({
     mode: 'uncontrolled',
     initialValues: {
@@ -10,7 +12,8 @@ export const CreateQuiz = () => {
     },
 
     validate: {
-      language: (value) => (/^\S+$/.test(value) ? null : 'Invalid input'),
+      language: (value) =>
+        /^.{2,30}$/.test(value) ? null : 'Input needs to be between 2 and 30 characters',
       difficulty: (value) => (value ? null : 'Must choose one option'),
     },
   });
@@ -18,14 +21,15 @@ export const CreateQuiz = () => {
   return (
     <Paper shadow='sm' radius='md' withBorder p='xl'>
       <Text size='xl' mb='md'>
-        Create a new language quiz
+        Create a new language quiz.
       </Text>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+      <form onSubmit={form.onSubmit((values) => fetchQuiz(values))}>
         <TextInput
           withAsterisk
           label='Language'
           placeholder='Swenglish'
           mb='md'
+          autoComplete='off'
           key={form.key('language')}
           {...form.getInputProps('language')}
         />
@@ -39,11 +43,11 @@ export const CreateQuiz = () => {
           <Group>
             <Radio value={'beginner'} label='Beginner' />
             <Radio value={'intermediate'} label='Intermediate' />
-            <Radio value={'proelite'} label='Proelite' />
+            <Radio value={'advanced'} label='ProElite' />
           </Group>
         </Radio.Group>
         <Group justify='center' mt='md'>
-          <Button fullWidth type='submit'>
+          <Button fullWidth type='submit' rightSection={<IconSquarePlus />}>
             Create
           </Button>
         </Group>
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,11 +1,26 @@
 import { CreateQuiz } from '../components/CreateQuiz';
-import { Text, Stack } from '@mantine/core';
+import { Text, Stack, LoadingOverlay, Title } from '@mantine/core';
+import { useState } from 'react';
 export const HomePage = () => {
+  const [visible, setVisible] = useState(false);
+  const fetchQuiz = (values) => {
+    setVisible(true);
+    setTimeout(() => {
+      setVisible(false);
+    }, 2000);
+    console.log(values);
+
+    //Route to /quiz
+  };
   return (
     <>
+      <LoadingOverlay visible={visible} />
       <Stack align='center' justify='center'>
-        <Text size='xl'>Welcome to the Polygot Lingo Venture</Text>
-        <CreateQuiz />
+        <Title order={1}>Welcome to the Polygot Lingo Venture</Title>
+        <Text>
+          You can browse between different quizzes in the menu or create a new quiz below.
+        </Text>
+        <CreateQuiz fetchQuiz={fetchQuiz} />
       </Stack>
     </>
   );
