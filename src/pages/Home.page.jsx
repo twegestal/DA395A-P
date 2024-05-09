832d13dc31c7c20c53ca8a7068b3cf7135439340
Axel nilsson <axel.nilsson01@gmail.com>
2024-05-09 12:37:22 +0200

    Create component added

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,3 +1,12 @@
+import { CreateQuiz } from '../components/CreateQuiz';
+import { Text, Stack } from '@mantine/core';
 export const HomePage = () => {
-  return <p>hej?</p>;
+  return (
+    <>
+      <Stack align='center' justify='center'>
+        <Text size='xl'>Welcome to the Polygot Lingo Venture</Text>
+        <CreateQuiz />
+      </Stack>
+    </>
+  );
 };
