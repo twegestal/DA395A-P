8af5614a2830dba5ce2c040f135d03b3dab0990d
Axel nilsson <axel.nilsson01@gmail.com>
2024-05-09 21:58:56 +0200

    List item created

---
+++ src/components/QuizListItem.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import { Group, Button, Text, Accordion, Center, Box } from '@mantine/core';
+import { IconX, IconCheck, IconMinus } from '@tabler/icons-react';
+import { failColor, successColor } from '../utils/constants';
+
+export const QuizListItem = ({ item }) => {
+  return (
+    <Accordion.Item value={item.id} key={item.title}>
+      <Center>
+        <Accordion.Control
+          icon={
+            item.isFinished == null ? (
+              <IconMinus size={30} color='gray' />
+            ) : !item.isFinished ? (
+              <IconX size={30} color={failColor} />
+            ) : (
+              <IconCheck size={30} color={successColor} />
+            )
+          }
+        >
+          <Group wrap='nowrap'>
+            <Box>
+              <Text>{item.title}</Text>
+              <Text size='sm' c='dimmed' fw={400}>
+                {item.description}
+              </Text>
+            </Box>
+          </Group>
+        </Accordion.Control>
+        <Box>
+          <Button variant='outline' mr='md'>
+            Start
+          </Button>
+        </Box>
+      </Center>
+    </Accordion.Item>
+  );
+};
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const successColor = '#00b341';
+export const failColor = '#ff2825';
